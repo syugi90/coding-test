31087876117bfefb937c2995686568f8093f662e
허미숙 <altnr23@gmail.com>
2020-09-09 06:31:17 +0000

    단계별 3단계

--- src/project/Boj.java
+++ src/project/Boj.java
@@ -1,7 +1,169 @@
 package project;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Boj{
 
+    //===============================================================
+    // 2020-09-09 (수)
+    //===============================================================
+    //구구단
+    public void q2739(int n){
+        for(int i=1; i<10; i++){
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+    }
+    
+    //빠른 A+B
+    public void q15552() throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine().trim());
+        for (int i=0; i < n; i++) {
+            String text = br.readLine();
+            String[] word = text.split(" ");
+            int a = Integer.parseInt(word[0]);
+            int b = Integer.parseInt(word[1]);
+            bw.write((a+b) + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
+            
+        
+    }
+    
+    //N 찍기
+    public void q2741() throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine().trim());
+        for (int i=1; i <= n; i++) {
+			
+            bw.write(i + "\n");
+        }
+		
+        bw.flush();
+        bw.close();
+		
+    }
+	
+	 //A+B - 7
+    public void q11021() throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine().trim());
+		
+        for (int i=1; i <= n; i++) {
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #"+i+": "+(a+b)+"\n");
+        }
+		
+        bw.flush();
+        bw.close();
+		
+    }
+	
+	 //A+B - 8
+    public void q11022() throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine().trim());
+		
+        for (int i=1; i <= n; i++) {
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #"+i+": "+a+" + "+b+" = "+(a+b)+"\n");
+        }
+		
+        bw.flush();
+        bw.close();
+		
+    }
+	
+	//별 찍기 - 1
+    public void q2438() throws IOException {
+            
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		int n = Integer.parseInt(br.readLine().trim());
+		
+        for (int i=1; i <= n; i++) {
+			for (int j=1; j<=i; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+        }
+		
+        bw.flush();
+        bw.close();
+		
+    }
+    
+	//별 찍기 - 2
+    public void q2439() throws IOException {
+            
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		int n = Integer.parseInt(br.readLine().trim());
+		
+        for (int i=1; i <= n; i++) {
+			for (int k=1; k<=n-i; k++) {
+				bw.write(" ");
+			}
+			for (int j=1; j<=i; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+        }
+		
+        bw.flush();
+        bw.close();
+		
+    }
+	
+	//X보다 작은 수 
+	public void q10871() throws IOException {
+            
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		
+		String str = br.readLine();
+		
+		StringTokenizer st = new StringTokenizer(str);
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+	
+		String str2 = br.readLine();
+		StringTokenizer arr = new StringTokenizer(str2);
+		for (int i=0; i<N; i++) {
+			
+			int n = Integer.parseInt(arr.nextToken());
+			if(n < X){
+				bw.write(n+ " ");
+			}
+		}
+		
+        bw.flush();
+        bw.close();
+		
+    }
+	
     //===============================================================
     // 2020-09-08 (화)
     //===============================================================
--- src/project/Main.java
+++ src/project/Main.java
@@ -1,10 +1,11 @@
 package project;
 
 import java.util.*;
-    
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         
         
         Boj boj = new Boj();
@@ -21,14 +22,38 @@ public class Main {
          // if(!getTest("12,-5",4,boj.q14681(12,-5))) return;
          
         //2884번 알람시계
-        if(!ts.getTest("10,10","9 25",boj.q2884(10,10))) return;
-        if(!ts.getTest("9,25","8 40",boj.q2884(9,25)))  return;
-        if(!ts.getTest("0,30","23 45",boj.q2884(0,30)))  return;
-        if(!ts.getTest("23,45","23 0",boj.q2884(23,45)))  return;
-        if(!ts.getTest("23,40","22 55",boj.q2884(23,40)))  return;
-        if(!ts.getTest("22,55","22 10",boj.q2884(22,55)))  return;
-        if(!ts.getTest("0,0","23 15",boj.q2884(0,0)))  return;
+        // if(!ts.getTest("10,10","9 25",boj.q2884(10,10))) return;
+        // if(!ts.getTest("9,25","8 40",boj.q2884(9,25)))  return;
+        // if(!ts.getTest("0,30","23 45",boj.q2884(0,30)))  return;
+        // if(!ts.getTest("23,45","23 0",boj.q2884(23,45)))  return;
+        // if(!ts.getTest("23,40","22 55",boj.q2884(23,40)))  return;
+        // if(!ts.getTest("22,55","22 10",boj.q2884(22,55)))  return;
+        // if(!ts.getTest("0,0","23 15",boj.q2884(0,0)))  return;
         
+        //2739번 구구단
+        //boj.q2739(2);
+        
+        // q15552 빠른 A+B
+         // boj.q15552();     
+	
+		//2741번 N찍기
+		//boj.q2741();
+		
+		//11021번 A+B-7
+		//boj.q11021();
+		
+		//11022번 A+B-8
+		//boj.q11022();
+		
+		//2438번 별찍기 
+		//boj.q2438();
+			
+		//2459번 별찍기 2
+		//boj.q2439();
+		
+		//10871 번 X보다 작은 수
+		boj.q10871();
+		
         System.out.println("================[완료!!]=======================");
    }
 
