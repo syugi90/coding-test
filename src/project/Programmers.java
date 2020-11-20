560ac4d4ad68cfe1d2033798c6eacb45e70c0eae
altnr23 <altnr23@gmail.com>
2020-11-20 08:30:38 +0000

    add programmers

---
+++ src/project/Programmers.java
@@ -0,0 +1,25 @@
+import java.util.*; 
+
+//두개뽑아서 더하기 
+import java.util.*; 
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        Set<Integer> sumNumber = new TreeSet();
+        for(int i=0; i<numbers.length; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                sumNumber.add(numbers[i]+numbers[j]);
+            }
+        }
+        
+        answer = new int[sumNumber.size()];
+        int index = 0;
+        Iterator it = sumNumber.iterator();
+        while(it.hasNext()){
+            answer[index] = (int)it.next();
+            index++;
+        }
+        return answer;
+    }
+}
